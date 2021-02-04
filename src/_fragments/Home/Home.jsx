b08c1fa11bcb4ca550341e85386f245d6bff794e
IceBunny <vigneshprabhakar.s@gmail.com>
2021-02-04 15:03:22 +0530

    v0.0 home screen 60%

--- src/_fragments/Home/Home.jsx
+++ src/_fragments/Home/Home.jsx
@@ -6,6 +6,11 @@ import "./Home.scss";
 
 const welcomeMsg = `if you can't stop thinking about it... buy it.`;
 
+const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat.`;
+
 const HomeFragment = () => {
   let carousalItems = [
     { src: "/images/home_06.jpg" },
@@ -14,6 +19,13 @@ const HomeFragment = () => {
     { src: "/images/home_03.jpg" },
     { src: "/images/home_02.jpg" },
     { src: "/images/home_01.jpg" },
+  ];
+
+  const products = Array(5).fill("");
+
+  let categories = [
+    { title: "home", description: description, featuredProducts: products },
+    { title: "men", description: description, featuredProducts: products },
   ];
 
   const carouselOptions = {
@@ -38,6 +50,39 @@ const HomeFragment = () => {
         <div className="bill-board-caption">
           <div>{welcomeMsg}</div>
         </div>
+      </div>
+
+      <div className="categories-sec">
+        {categories.map(({ title, description, featuredProducts }, i) => (
+          <div className="category">
+            <div
+              className={
+                "category-description" + ((i % 2 !== 0)
+                  ? " category-description-reverse"
+                  : "")
+              }
+            >
+              <div className="shop-btn no-s">Shop all</div>
+              <div className="header">
+                <div className="title">{title}</div>
+                <div className="description">{description}</div>
+              </div>
+            </div>
+
+            <div className="category-products">
+              <div className="category-image"></div>
+              <div className="products-list">
+                {featuredProducts.map(() => (
+                  <div className="product-item">
+                    <div className="name">Product Name</div>
+                    <div className="price">View Details - ₹999.99</div>
+                  </div>
+                ))}
+              </div>
+              <div className="shop-btn no-s">Shop all</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
